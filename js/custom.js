639d9005be5a90c4c40474f569081453151fa9ac
Sarah <sar.himmler@gmail.com>
2018-10-07 10:53:40 -0400

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -73,7 +73,7 @@ var index=0;
 $(document).scroll(function(){
 	var top = $('.technical').height()-$(window).scrollTop();
 	console.log(top)
-	if(top<-300){
+	if(top<-100){
 		if(index==0){	
 			
 			$('.chart').easyPieChart({
